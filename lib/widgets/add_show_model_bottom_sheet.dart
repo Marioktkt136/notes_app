0ed7c37e7c03f1e41913427742145f092dcc52c2
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-04-07 10:50:39 +0200

    user input validation & take inputs

--- lib/widgets/add_show_model_bottom_sheet.dart
+++ lib/widgets/add_show_model_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -11,30 +10,69 @@ class AddNotesBottomSheet extends StatelessWidget {
     return SingleChildScrollView(
       child: Container(
         padding: const EdgeInsets.all(24),
-        child: const Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Title',
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            CustomTextField(
-              hintText: 'Description',
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hintText: 'Title',
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              description = value;
+            },
+            hintText: 'Description',
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,29 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: kPrimaryColor,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black45,
-              offset: Offset(1, 1),
-            )
-          ]),
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontWeight: FontWeight.w400,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: kPrimaryColor,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black45,
+                offset: Offset(1, 1),
+              )
+            ]),
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.w400,
+            ),
           ),
         ),
       ),
