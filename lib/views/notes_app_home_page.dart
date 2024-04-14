0bfe5717e9bec63029b04c676e873f1bc5e8ad99
Mario Naiem Bedrous <marioktkt136@gmail.com>
2024-04-14 16:11:25 +0200

    Create fetch notes method

--- lib/views/notes_app_home_page.dart
+++ lib/views/notes_app_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes/notes_cubit.dart';
 import 'package:notes_app/widgets/add_show_model_bottom_sheet.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
@@ -10,43 +12,46 @@ class NotesAppHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        elevation: 30,
-        splashColor: Colors.blue.withOpacity(0.4),
-        backgroundColor: Colors.green.withOpacity(1),
-        hoverColor: Colors.blueAccent.withOpacity(0.4),
-        onPressed: () {
-          showModalBottomSheet(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            elevation: 3,
-            context: context,
-            builder: (context) {
-              return const AddNotesBottomSheet();
-            },
-          );
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          elevation: 30,
+          splashColor: Colors.blue.withOpacity(0.4),
+          backgroundColor: Colors.green.withOpacity(1),
+          hoverColor: Colors.blueAccent.withOpacity(0.4),
+          onPressed: () {
+            showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              elevation: 3,
+              context: context,
+              builder: (context) {
+                return const AddNotesBottomSheet();
+              },
+            );
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        child: Column(
-          children: [
-            const CustomAppBar(
-              title: 'Notes',
-              icon: Icons.search,
-            ),
-            Expanded(
-              child: NotesListView(),
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
+          child: Column(
+            children: [
+              const CustomAppBar(
+                title: 'Notes',
+                icon: Icons.search,
+              ),
+              Expanded(
+                child: NotesListView(),
+              ),
+            ],
+          ),
         ),
       ),
     );
