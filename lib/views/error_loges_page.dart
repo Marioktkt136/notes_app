f232077cb0482fcfc94800d34d03e7a93e4c62fd
Mario Naiem Bedrous <62382441+Marioktkt136@users.noreply.github.com>
2024-04-18 14:57:49 +0200

    ss

--- lib/views/error_loges_page.dart
+++ lib/views/error_loges_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar_for_errors_Page.dart';
 
 class ErrorLogsPage extends StatelessWidget {
   const ErrorLogsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [
+          CustomAppBarForErrorPage(
+            title: 'Error Logs',
+            icon: Icons.arrow_back,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ]),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_app_bar_for_errors_Page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
+
+class CustomAppBarForErrorPage extends StatelessWidget {
+  const CustomAppBarForErrorPage(
+      {super.key, required this.title, required this.icon, this.onPressed});
+  final String title;
+  final IconData icon;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 70),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+          const Spacer(),
+          CustomSearchIcon(
+            onPressed: onPressed,
+            icon: icon,
+          ),
+        ],
+      ),
+    );
+  }
+}
